29139212f41379cd7e9885e939991a82d3171b33
r-tamura <rtamura30@gmail.com>
2017-05-08 00:11:46 +0900

    不要なフォントファイルを削除

--- components/Tag.js
+++ components/Tag.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import './Tag.css'
 
 const Tag = ({children = ''} = {}) => (
-  <div className={'tag'}>
+  <div className={'post-tag'}>
     {children}
   </div>
 )

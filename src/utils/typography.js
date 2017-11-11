900a037354e81580cf56617d2a999b6e79142ec0
r-tamura <rtamura30@gmail.com>
2017-11-11 22:22:21 +0900

    デバイス幅が小さいディスプレイのレイアウトを修正

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,14 @@
 import Typography from 'typography'
 import sutroTheme from 'typography-theme-sutro'
 
-// ベースはsutroテーマ
-// 基本フォントサイズのみ 18px -> 16pxに変更
-sutroTheme.baseFontSize = "16px"
 
-const typography = new Typography(sutroTheme)
+// ベースはsutroテーマ
+const typography = new Typography({
+  ...sutroTheme,
+  // 基本フォントサイズのみ 18px -> 16pxに変更
+  baseFontSize: "16px",
+  // overrideThemeStyles: ({ rhythm }, options, styles) => ({
+  // })
+})
 
 module.exports = typography

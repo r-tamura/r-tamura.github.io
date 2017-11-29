cb619d0589cc9c4a2b8a083243eeb3af46f5a6e3
r-tamura <rtamura30@gmail.com>
2017-11-29 23:59:26 +0900

    .circleciディレクトリがデプロイできない不具合修正

--- utils/deploy.js
+++ utils/deploy.js
@@ -1,19 +1,22 @@
 /* eslint no-console: 0 */
 const ghpages = require('gh-pages')
 const path = require('path')
-const moment = require('moment')
+const { promisify } = require('util')
+const { DateTime } = require('luxon')
 
 const options = {
   repo: 'git@github.com:r-tamura/r-tamura.github.io.git',
   branch: 'master',
-  message: `Site updated: ${moment().format('YYYY-MM-DD hh:mm:ss')}`
+  message: `Site updated: ${DateTime.local().setZone('Asia/Tokyo').toString()}`,
+  dotfiles: true,
 }
 
-ghpages.publish(path.resolve(__dirname, '../public'), options, err => {
-  if (err) {
+const publish = promisify(ghpages.publish)
+
+publish(path.resolve(__dirname, '../public'), options)
+  .then(_ => console.log('Deploy finished successfully'))
+  .catch(err => {
     console.log(err)
     throw new Error('Deploy failed')
-  } else {
-    console.log('Deploy finished successfully')
-  }
-})
+  })
+

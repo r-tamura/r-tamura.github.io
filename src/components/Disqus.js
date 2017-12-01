1b2d2424c84c0a9db51dfce0c4c916a14cac4122
r-tamura <rtamura30@gmail.com>
2017-12-01 20:21:03 +0900

    Disqus導入

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -2,9 +2,10 @@ import React from 'react'
 import propTypes from 'prop-types'
 import ReactDisqusComments from 'react-disqus-comments'
 
-export default ({ siteUrl, articleId }) => 
+export default ({ shortname, articleId, title }) => 
   <ReactDisqusComments
     shortname={"rtams-tech-blog"}
     identifier={articleId}
+    title={title}
     category_id={'tech'}
   />
